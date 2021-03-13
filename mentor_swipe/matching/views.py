13f3a88df0c8e91567955d24bb80a9e817853676
Dylan Carter <dccarte3@asu.edu>
2021-03-12 22:51:09 -0700

    Added landing page, registration, and login page

--- mentor_swipe/matching/views.py
+++ mentor_swipe/matching/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Home</h1>')
+def landing(request):
+    return render(request, 'matching/landing.html')
--- mentor_swipe/mentor_swipe/settings.py
+++ mentor_swipe/mentor_swipe/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'matching.apps.MatchingConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
